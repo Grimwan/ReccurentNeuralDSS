e2b9c7cfa03c045b8cfeeeea8789768b84298bff
Unknown <deankulenovic@gmail.com>
2019-02-18 11:22:08 +0100

    Now we can have borders yeeeeeeaaay

--- ReccurentNeuralDSS/ReccurentNeuralDSS/ImageLoader.py
+++ ReccurentNeuralDSS/ReccurentNeuralDSS/ImageLoader.py
@@ -6,7 +6,7 @@ import cv2
 import random
 import pickle 
 class ImageLoader(object):
-    def imageReader(dataDir, trainingDir, resultDir, resize):
+    def imageReader(dataDir, trainingDir, resultDir, resize,Crop):
         training_data = []
         for category in trainingDir:
             path = os.path.join(dataDir, category) # path to img/training or pixel-level-gt/training dir
@@ -18,6 +18,18 @@ class ImageLoader(object):
                 try:
                     imgTraining_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_COLOR)
                     imgResult_array = cv2.imread(os.path.join(secondPath,newImg[i]),cv2.IMREAD_COLOR)
+                    max_y,max_x = imgTraining_array.shape[:2]
+                    if Crop[0] != 0:
+                        border_y = 0
+                        if max_y % Crop[2] != 0:
+                            border_y = (Crop[2] - (max_y % Crop[2]) + 1) // 2
+                            imgTraining_array = cv2.copyMakeBorder(imgTraining_array,border_y,border_y,0,0, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                            imgResult_array = cv2.copyMakeBorder(imgResult_array,border_y,border_y,0,0, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                        border_x = 0
+                        if max_x % Crop[1] != 0:
+                            border_x = (Crop[1] - (max_x % Crop[1]) + 1) // 2
+                            imgTraining_array = cv2.copyMakeBorder(imgTraining_array,0,0,border_x,border_x, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                            imgResult_array = cv2.copyMakeBorder(imgResult_array,0,0,border_x,border_x, cv2.BORDER_CONSTANT, value=[255, 255, 255])
                     if resize[0] != 0:
                         newTraining_array = cv2.resize(imgTraining_array, (resize[1],resize[2]))
                         newResult_array = cv2.resize(imgResult_array, (resize[1],resize[2]))
@@ -29,8 +41,8 @@ class ImageLoader(object):
                     pass
         return training_data
 
-    def saveImages(dataDir, trainingDir, resultDir, resize,shuffleData,pickleOutX,pickleOutY):
-        training_data = ImageLoader.imageReader(dataDir, trainingDir, resultDir, resize)
+    def saveImages(dataDir, trainingDir, resultDir, resize,shuffleData,pickleOutX,pickleOutY,WheretosavePickleData):
+        training_data = ImageLoader.imageReader(dataDir, trainingDir, resultDir, resize,[1,128,128])
         if(bool(shuffleData)):
             random.shuffle(training_data)
         x = [] #trainingdata
@@ -44,18 +56,56 @@ class ImageLoader(object):
         else:
             x = np.array(x)
             y = np.array(y)
-        pickle_out = open(pickleOutX,"wb")
+
+        im2 = x.copy()
+        im2[:,:, :, 0] = x[:, :, :, 2]
+        im2[:,:, :, 2] = x[:, :, :, 0]
+        x = im2
+        ANotherImg = y.copy()
+        ANotherImg[:,:, :, 0] = y[:,:, :, 2]
+        ANotherImg[:,:, :, 2] = y[:,:, :, 0]
+        y = ANotherImg
+        pickle_out = open(WheretosavePickleData+pickleOutX,"wb")
         pickle.dump(x,pickle_out)
         pickle_out.close()
 
-        pickle_out = open(pickleOutY,"wb")
+        pickle_out = open(WheretosavePickleData+pickleOutY,"wb")
         pickle.dump(y,pickle_out)
         pickle_out.close()
         print("Finished saving images to pickle x for "+pickleOutX+"  y for "+pickleOutY)
 
-    def loadSavedImage(inX,inY):
-        pickle_in = open(inX,"rb")
+    def loadSavedImage(WhereToload,inX,inY):
+        pickle_in = open(WhereToload+inX,"rb")
         x_train = pickle.load(pickle_in)
-        pickle_in = open(inY,"rb")
+        pickle_in = open(WhereToload+inY,"rb")
         y_train = pickle.load(pickle_in)
         return [x_train,y_train]
+
+    def imageReaderCrop(dataDir,trainingDir,resultDir,Crop):
+        training_data = []
+        for category in trainingDir:
+            path = os.path.join(dataDir, category) # path to img/training or pixel-level-gt/training dir
+            secondPath = os.path.join(dataDir,resultDir[trainingDir.index(category)])
+            #class_num = CATEGORIES.index(category)
+            newImg =  os.listdir(secondPath)
+            i = 0;
+            for img in os.listdir(path):
+                try:
+                    imgTraining_array = cv2.imread(os.path.join(path,img), cv2.IMREAD_COLOR)
+                    imgResult_array = cv2.imread(os.path.join(secondPath,newImg[i]),cv2.IMREAD_COLOR)
+                    max_y,max_x = imgTraining_array.shape[:2]
+                    border_y = 0
+                    if max_y % Crop.y != 0:
+                        border_y = (Crop.y - (max_y % Crop.y) + 1) // 2
+                        imgTraining_array = cv2.copyMakeBorder(imgTraining_array,border_y,border_y,0,0, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                        imgResult_array = cv2.copyMakeBorder(imgResult_array,border_y,border_y,0,0, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                    border_x = 0
+                    if max_x % Crop.x != 0:
+                        border_x = (Crop.x - (max_x % Crop.x) + 1) // 2
+                        imgTraining_array = cv2.copyMakeBorder(imgTraining_array,0,0,border_x,border_x, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                        imgResult_array = cv2.copyMakeBorder(imgResult_array,0,0,border_x,border_x, cv2.BORDER_CONSTANT, value=[255, 255, 255])
+                    training_data.append([imgTraining_array,imgResult_array])
+                    i= i+1
+                except Exception as e:
+                    pass
+        return training_data
--- ReccurentNeuralDSS/ReccurentNeuralDSS/ReccurentNeuralDSS.py
+++ ReccurentNeuralDSS/ReccurentNeuralDSS/ReccurentNeuralDSS.py
@@ -4,13 +4,21 @@
 import matplotlib.pyplot as plt
 import ImageLoader as Loader
 DATADIR = "../Imagesfiles"
-Training = ["CB55/img/training", "CS18/img/training","CS863/img/training"]
-Result = ["CB55/pixel-level-gt/training", "CS18/pixel-level-gt/training", "CS863/pixel-level-gt/training"]
-#Loader.ImageLoader.saveImages(DATADIR,Training,Result,[1,2000,2000],True,"Testsavehere.x","TestSaveHere.y");
-[x_train,y_train] = Loader.ImageLoader.loadSavedImage("Testsavehere.x","TestSaveHere.y")
+#Training = ["CB55/img/training", "CS18/img/training","CS863/img/training"]
+#Result = ["CB55/pixel-level-gt/training", "CS18/pixel-level-gt/training", "CS863/pixel-level-gt/training"]
+Training = ["DeansTestmap/img/training"]
+Result = ["DeansTestmap/pixel-level-gt/training"]
+Loader.ImageLoader.saveImages(DATADIR,Training,Result,[0,2000,2000],True,"Testsavehere.x","TestSaveHere.y","../PickleSave/");
+[x_train,y_train] = Loader.ImageLoader.loadSavedImage("../PickleSave/","Testsavehere.x","TestSaveHere.y")
 
-print(x_train.shape[2])
-print(y_train.shape)
-plt.imshow(x_train[42])
+
+#im2 = x_train[42].copy()
+#im2[:, :, 0] = x_train[42][:, :, 2]
+#im2[:, :, 2] = x_train[42][:, :, 0]
+#x_train[42] = im2
+max_y,max_x = x_train[0].shape[:2]
+print(max_y)
+print(max_x)
+plt.imshow(x_train[0])
 plt.show()
 
