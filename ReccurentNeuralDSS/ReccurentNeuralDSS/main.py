52d4f80b9ad06d20b386376ea22a4f49b32e4aa9
Jakobzzz <jakob.ahrneteg@gmail.com>
2019-02-20 17:05:46 +0100

    Cleaned up model class, added utility to save and load model weights

--- ReccurentNeuralDSS/ReccurentNeuralDSS/main.py
+++ ReccurentNeuralDSS/ReccurentNeuralDSS/main.py
@@ -1,9 +1,6 @@
-#### coding rules ####
-#liten bokstav på funktioner
-#liten bokstav på variabler
 import matplotlib.pyplot as plt
 import utils.imageLoader as loader
-import model.model as model
+from model.model import Model
 
 DATADIR = "../Imagesfiles"
 #Training = ["CB55/img/training", "CS18/img/training","CS863/img/training"]
@@ -13,44 +10,47 @@ Result = ["DeansTestmap/pixel-level-gt/training"]
 Xsize = 32
 Ysize= 32
 
-loader.ImageLoader.saveImagesToPickle(DATADIR, Training, Result, [0,2000,2000], [1,Xsize,Ysize], True, "Testsavehere.x", "TestSaveHere.y", "../PickleSave/");
-[x_train,y_train] = loader.ImageLoader.loadSavedImage("../PickleSave/", "Testsavehere.x", "TestSaveHere.y")
+def main():
+    loader.ImageLoader.saveImagesToPickle(DATADIR, Training, Result, [0,2000,2000], [1,Xsize,Ysize], True, "X.pickle", "y.pickle", "../output/");
+    [x_train,y_train] = loader.ImageLoader.loadSavedImage("../output/", "X.pickle", "y.pickle")
+    
+    print(x_train.shape)
+    
+    x_train = x_train.reshape(y_train.shape[0], Xsize*Ysize*3)
+    x_train = x_train.astype('float32') / 255
+    y_train = y_train.reshape(y_train.shape[0], Xsize*Ysize*3)
+    y_train = y_train.astype('float32') / 255
+    
+    loader.ImageLoader.removingOnlyDarkpictures(y_train,0)
+    
+    model = Model.build_model(Xsize*Ysize*3)
+    model.fit(x_train,y_train, epochs=1, batch_size=20)
+    ynew = model.predict(x_train)
+    
+    
+    x_train = x_train.reshape(x_train.shape[0],Xsize,Ysize,3)
+    x_train = x_train.astype('float32') * 255
+    y_train = y_train.reshape(y_train.shape[0],Xsize,Ysize,3)
+    y_train = y_train.astype('float32') * 255
+    
+    ynew = ynew.reshape(ynew.shape[0],Xsize,Ysize,3)
+    ynew = ynew.astype('float32') * 255
+    
+    img = loader.ImageLoader.combine_images(ynew, 6496, 4872)
+    plt.imshow(img)
+    plt.show()
+    
+    #im2 = x_train[42].copy()
+    #im2[:, :, 0] = x_train[42][:, :, 2]
+    #im2[:, :, 2] = x_train[42][:, :, 0]
+    #x_train[42] = im2
+    #max_y,max_x = x_train[0].shape[:2]
+    #print(max_y)
+    #print(max_x)
+    #print(x_train.shape)
+    #plt.imshow(img)
+    #plt.show()
 
-print(x_train.shape)
-
-x_train = x_train.reshape(y_train.shape[0], Xsize*Ysize*3)
-x_train = x_train.astype('float32') / 255
-y_train = y_train.reshape(y_train.shape[0], Xsize*Ysize*3)
-y_train = y_train.astype('float32') / 255
-
-loader.ImageLoader.removingOnlyDarkpictures(y_train,0)
-
-Model = model.Model.build_Standard_NN_model(Xsize*Ysize*3,Xsize*Ysize*3)
-Model.fit(x_train,y_train, epochs = 5, batch_size=20)
-ynew = Model.predict(x_train)
-
-
-x_train = x_train.reshape(x_train.shape[0],Xsize,Ysize,3)
-x_train = x_train.astype('float32') * 255
-y_train = y_train.reshape(y_train.shape[0],Xsize,Ysize,3)
-y_train = y_train.astype('float32') * 255
-
-ynew = ynew.reshape(ynew.shape[0],Xsize,Ysize,3)
-ynew = ynew.astype('float32') * 255
-
-img = loader.ImageLoader.combine_images(ynew, 6496, 4872)
-plt.imshow(img)
-plt.show()
-
-
-#im2 = x_train[42].copy()
-#im2[:, :, 0] = x_train[42][:, :, 2]
-#im2[:, :, 2] = x_train[42][:, :, 0]
-#x_train[42] = im2
-#max_y,max_x = x_train[0].shape[:2]
-#print(max_y)
-#print(max_x)
-#print(x_train.shape)
-#plt.imshow(img)
-#plt.show()
+if __name__ == "__main__":
+    main()
 
