51c8035a7012a2e0168981f36a764de5af02f148
Unknown <deankulenovic@gmail.com>
2019-04-29 22:05:56 +0200

    swag

--- ReccurentNeuralDSS/ReccurentNeuralDSS/LayoutAnalysis/RunMe.bat
+++ ReccurentNeuralDSS/ReccurentNeuralDSS/LayoutAnalysis/RunMe.bat
@@ -1,2 +1,9 @@
 @echo off
-java -jar LayoutAnalysisEvaluator.jar -gt gt_image.png -p prediction_image.png
+cd gtImages
+ren *.png gt_image.*
+cd ..
+cd predictionImages
+ren *.png prediction_image.*
+cd ..
+
+java -jar LayoutAnalysisEvaluator.jar -gt gtImages/gt_image.png -p predictionImages/prediction_image.png > Output.txt
